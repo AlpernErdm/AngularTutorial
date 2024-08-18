49fcc19c3cd50d3ee1b12377a594d9e033337bc1
Alperen Erdem <alpernerdm@gmail.com>
2024-08-18 16:53:39 +0300

    Pipes yazıldı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularTutorial';
 }
+
+class Alperen{
+name:string;
+number:number;
+createdDate:Date;
+
+constructor(name:string,number:number,createdDate:Date) {
+  this.name=name;
+  this.number=number;
+  this.createdDate=createdDate;
+
+}
+Topla(a1:number,a2:number):number //dönüş tipi belirttiğim için return ettiğim number olmalı
+{
+  return a1+a2;
+}
+
+Topla2(a1:number,a2:number) //dönüş tipi belirtmediğim  için return ettiğim herhnagi bişi olabilir
+{
+  var b1=1;
+  var b2=2;
+
+}
+}
+
+
